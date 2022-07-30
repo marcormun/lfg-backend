38a65a0ea021f045eaec554681052dcd5d554377
marcormun <mcordontrabajo@gmail.com>
2022-07-30 18:49:37 +0200

    feat: user role many to many relation ended

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
